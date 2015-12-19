989530e39cd45b52b81357d5fd2a955922569a8b
Paul <guitarmansam87@gmail.com>
2015-12-18 22:43:16 -0500

    added

---
+++ Python/test_filtertools.py
@@ -0,0 +1,25 @@
+import FilterTools as ft
+import BinVectorTools as bvt
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import warnings
+warnings.simplefilter(action = "ignore", category = FutureWarning)
+
+Ns = 10
+Rs = 10
+sens = 0.005
+
+## Generate input vector and add noise
+X = bvt.OversampleBinVector(bvt.GenerateBinVector(Ns, 0), Rs)
+wgn = sens*np.random.randn(1, Ns*Rs)[0]
+Xn = X + wgn
+
+filt1 = ft.FilterTools(1, 3, 0.6, 'low')
+Yc = filt1.filter(X)
+Ycn = filt1.filter(Xn)
+
+print filt1.rise_time()
+print filt1.overshoot()
+print filt1.rtfilter(7, np.arange(3))
+print filt1.get_coef()['a']
