e5507935b35648df7cec63598879f3270b25f7c5
Paul <guitarmansam87@gmail.com>
2015-12-19 12:11:09 -0500

    Cleaned up unnecessary variables

--- Python/PWMTools.py
+++ Python/PWMTools.py
@@ -1,10 +1,7 @@
 import RPi.GPIO as gpio
 import time 
 
-NUM_CYCLES = 5
-CENTER = 1500
-list_size = 10
-pw_array = [CENTER for i in range(list_size)]
+num_cycles = 5
 
 def measure_pw(pin):
 	"""Returns pulse width in seconds of a square wave on pin *pin*"""
@@ -29,8 +26,8 @@ def measure_freq(pin):
 	""" Returns the frequency in Hz of a square wave on pin *pin*"""
 	frequency = 0
 	start = time.time()
-	for impulse_count in range(NUM_CYCLES):
+	for impulse_count in range(num_cycles):
 		gpio.wait_for_edge(pin, gpio.FALLING)
 	duration = time.time() - start
-	frequency = NUM_CYCLES / duration
+	frequency = num_cycles / duration
 	return frequency
