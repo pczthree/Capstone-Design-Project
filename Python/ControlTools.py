b7f03f776c3617b7c1f61c5107a2c87472fdb613
Paul <guitarmansam87@gmail.com>
2015-12-22 21:23:57 -0500

    Started coding calibration sequence

--- Python/ControlTools.py
+++ Python/ControlTools.py
@@ -1,4 +1,5 @@
 from Adafruit_PWM_Servo_Driver import PWM
+import numpy as np
 import PWMTools as pwt
 import RPi.GPIO as gpio
 
@@ -18,12 +19,50 @@ class ControlSurface(object)
 		num_pulses = int(time / self.pulse_length)
 		pwm.setPWM(self.channel, 0, num_pulses)
 
+	def calibrate_ROM(self):
+		threshold = 50
+		x_ = self.neutral
+		#hold min
+		m = []
+		for ii in range (10):
+			x = pwt.measure_pw_us(channel)
+			# will usually throw away the first point
+			if np.absolute(x - x_) > threshold:
+				ii -= 1
+			else:
+				m.append(pwt.measure_pw_us(channel))
+			x_ = x
+		self.min = sum(m)/len(m)
+
+		#hold max
+		M = []
+		for ii in range (10):
+			x = pwt.measure_pw_us(channel)
+			# will usually throw away the first point
+			if np.absolute(x - x_) > threshold:
+				ii -= 1
+			else:
+				M.append(pwt.measure_pw_us(channel))
+			x_ = x
+		self.max = sum(M)/len(M)
+
+		c = []
+		for ii in range (10):
+			x = pwt.measure_pw_us(channel)
+			# will usually throw away the first point
+			if np.absolute(x - x_) > threshold:
+				ii -= 1
+			else:
+				c.append(pwt.measure_pw_us(channel))
+			x_ = x
+		self.center = sum(c)/len(c)
+
 class Servo(ControlSurface):
 	def __init__(self, channel, freq, neutral=1500):
 		super(Servo).__init__(self, channel, freq)
 		self.neutral = neutral
 
 class ESC(ControlSurface):
-	def __init__(self, channel, freq, neutral=1500):
+	def __init__(self, channel, freq, neutral=0):
 		super(ESC).__init__(self, channel, freq)
 		self.neutral = neutral
