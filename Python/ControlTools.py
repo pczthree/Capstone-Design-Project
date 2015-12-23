8fd659a9cef09fbc5424f648787863c3bf57f9d4
Paul Czeresko <pczthree@HAL9000.local>
2015-12-23 12:48:26 -0500

    Added bounds to set_pw function

--- Python/ControlTools.py
+++ Python/ControlTools.py
@@ -2,21 +2,28 @@ from Adafruit_PWM_Servo_Driver import PWM
 import numpy as np
 import PWMTools as pwt
 import RPi.GPIO as gpio
+import ServoTools as st
 
 class ControlSurface(object)
 
 	def __init__(self, channel, freq):
 		self.channel = channel
 		self.freq = freq
+		self.max = 2000
+		self.min = 1000
 		
 		pwm.setPWMFreq(freq)
-		self.pulse_length = 1000000 / (freq * 4096)
+		self.pulse_length = 1000000/(freq*4096)
 
 	def reset(self):
 		set_pw(self.channel, self.neutral)
 
 	def set_pw(self, time): # in us
-		num_pulses = int(time / self.pulse_length)
+		if time > self.max:
+			time = self.max
+		elif time < self.min:
+			time = self.min
+		num_pulses = int(time/self.pulse_length)
 		pwm.setPWM(self.channel, 0, num_pulses)
 
 	def calibrate_ROM(self):
@@ -63,6 +70,6 @@ class Servo(ControlSurface):
 		self.neutral = neutral
 
 class ESC(ControlSurface):
-	def __init__(self, channel, freq, neutral=0):
+	def __init__(self, channel, freq, neutral=1000):
 		super(ESC).__init__(self, channel, freq)
 		self.neutral = neutral
